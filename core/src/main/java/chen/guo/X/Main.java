2e74d4f9351ab047b812b78bc35de261b16645e8
Chen Guo <almightygc@gmail.com>
2016-06-11 11:34:48 -0700

    add log4j support

--- core/src/main/java/chen/guo/X/Main.java
+++ core/src/main/java/chen/guo/X/Main.java
@@ -1,11 +1,15 @@
 package chen.guo.X;
 
 import chen.guo.X.common.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+  private static Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
 
-    System.out.println("Hello World");
-    System.out.println(Test.NAME);
+    logger.info("Hello World");
+    logger.info(Test.NAME);
   }
 }
