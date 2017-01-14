7edaa6a597dd13cf8fdf50a0c3f703e8a05611d3
Chen Guo <almightygc@gmail.com>
2017-01-14 15:39:10 +0800

    Add quote

--- play-app/app/com/chen/guo/models/learn/Examples.scala
+++ play-app/app/com/chen/guo/models/learn/Examples.scala
@@ -6,10 +6,7 @@ case class Order(title: String)
 
 case class UserData(name: String, age: Int)
 
-case class Contact(firstname: String, lastname: String, company: Option[String], informations: Seq[ContactInformation])
 
-object Contact {
-  def save(contact: Contact): Int = 99
-}
 
-case class ContactInformation(label: String, email: Option[String], phones: List[String])
+
+
